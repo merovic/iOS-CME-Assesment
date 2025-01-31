52de37e4044112333ea21c6d79e0f6e7a46bc86f
amirmorsypaysky <amir.morsy@paysky.io>
2025-01-31 19:50:00 +0200

    Initial Commit

---
+++ cme-technical-assessment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  cme-technical-assessment
+//
+//  Created by Amir Morsy on 31/01/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ cme-technical-assessmentTests/cme_technical_assessmentTests.swift
@@ -0,0 +1,17 @@
+//
+//  cme_technical_assessmentTests.swift
+//  cme-technical-assessmentTests
+//
+//  Created by Amir Morsy on 31/01/2025.
+//
+
+import Testing
+@testable import cme_technical_assessment
+
+struct cme_technical_assessmentTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
